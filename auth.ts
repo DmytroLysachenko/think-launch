9950dd016fd4728bd359d6546baa556a7df7c035
DmytroLysachenko <dlysachenko98@gmail.com>
2025-01-15 14:31:24 +0100

    Fixed build

--- auth.ts
+++ auth.ts
@@ -8,28 +8,29 @@ import { writeClient } from "./sanity/lib/write-client";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   callbacks: {
-    async signIn({
-      user: { name, email, image },
-      profile: { id, login, bio },
-    }) {
-      const existingUser = await client
-        .withConfig({ useCdn: false })
-        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-          id,
-        });
-      if (!existingUser) {
-        await writeClient.create({
-          _type: "author",
-          id,
-          name,
-          username: login,
-          email,
-          image,
-          bio: bio || "",
-        });
-      }
+    async signIn({ user: { name, email, image }, profile }) {
+      try {
+        const existingUser = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+            id: profile?.id,
+          });
+        if (!existingUser) {
+          await writeClient.create({
+            _type: "author",
+            id: profile?.id,
+            name,
+            username: profile?.login,
+            email,
+            image,
+            bio: profile?.bio || "",
+          });
+        }
 
-      return true;
+        return true;
+      } catch {
+        return false;
+      }
     },
     async jwt({ token, account, profile }) {
       if (account && profile) {
